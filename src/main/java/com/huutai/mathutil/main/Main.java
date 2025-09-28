abf39f6a4be1ee375c108b7c47f25556d9e6a183
tainguyenhuu2005 <tainh.ce190284@gmail.com>
2025-09-28 12:34:03 +0700

    1st upload with maven project

---
+++ src/main/java/com/huutai/mathutil/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.huutai.mathutil.main;
+
+import com.huutai.mathutil.core.MathUtil;
+
+/**
+ *
+ * @author DLCD
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("5! = " + MathUtil.getFactorial(5));
+    }
+}
